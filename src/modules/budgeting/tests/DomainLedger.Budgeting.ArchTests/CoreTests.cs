b60833b2cacb1ec863352be378f689c25e539f40
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-07-22 23:55:57 +0530

    test(architecture): add tests for strongly typed enumerations

--- src/modules/budgeting/tests/DomainLedger.Budgeting.ArchTests/CoreTests.cs
+++ src/modules/budgeting/tests/DomainLedger.Budgeting.ArchTests/CoreTests.cs
@@ -67,4 +67,45 @@ public class CoreTests : TestBase
         //Assert
         Assert.True(testResult.IsSuccessful);
     }
+
+    [Fact]
+    public void StronglyTypedEnumerations_shouldBe_Sealed()
+    {
+        TestResult result = Types
+            .InAssembly(CoreAssembly)
+            .That()
+            .Inherit(typeof(Enumeration))
+            .Should()
+            .BeSealed()
+            .GetResult();
+
+        Assert.True(result.IsSuccessful);
+
+    }
+
+    [Fact]
+    public void StronglyTypedEnumerations_ShouldOnlyHve_PrivateConstructors()
+    {
+        //Arrange
+        IEnumerable<Type> enumerationTypes = Types
+            .InAssembly(CoreAssembly)
+            .That()
+            .Inherit(typeof(Enumeration))
+            .GetTypes();
+
+        List<Type> failingTypes = [];
+
+        foreach (Type enumerationType in enumerationTypes)
+        {
+            ConstructorInfo[] infos = enumerationType.GetConstructors(BindingFlags.Instance | BindingFlags.Public);
+
+            if (infos.Any())
+            {
+                failingTypes.Add(enumerationType);
+
+            }
+        }
+
+        Assert.Empty(failingTypes);
+    }
 }
