7483029aaa79008325a7c6f350676baa1808d8fa
Codidharma - Mandar Dharmadhikari <codidharma@gmail.com>
2025-07-22 23:53:13 +0530

    test(architecture): add tests for core layer and infrastructure layer

---
+++ src/modules/budgeting/tests/DomainLedger.Budgeting.ArchTests/CoreTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Reflection;
+
+namespace DomainLedger.Budgeting.ArchTests;
+
+public class CoreTests : TestBase
+{
+    [Fact]
+    public void Entities_ShouldBe_SealedClasses()
+    {
+        //Act
+        TestResult testResult = Types
+            .InAssembly(CoreAssembly)
+            .That()
+            .Inherit(typeof(Entity))
+            .Should()
+            .BeSealed()
+            .And()
+            .BeClasses()
+            .GetResult();
+
+        //Assert
+        Assert.True(testResult.IsSuccessful);
+    }
+
+    [Fact]
+    public void Entities_ShouldOnlyHave_PrivateConstructor()
+    {
+        //Arrange
+        IEnumerable<Type> entities = Types
+            .InAssembly(CoreAssembly)
+            .That()
+            .Inherit(typeof(Entity))
+            .GetTypes();
+
+        List<Type> failingTypes = [];
+
+        foreach (Type entity in entities)
+        {
+            ConstructorInfo[] constructorInfos = entity.GetConstructors(BindingFlags.Public | BindingFlags.Instance);
+
+            if (constructorInfos.Any())
+            {
+                failingTypes.Add(entity);
+            }
+        }
+
+        //Assert
+        Assert.Empty(failingTypes);
+
+
+    }
+
+    [Fact]
+    public void Aggregates_ShouldBe_SealedClasses()
+    {
+        //Act
+        TestResult testResult = Types
+            .InAssembly(CoreAssembly)
+            .That()
+            .ImplementInterface(typeof(IAggregateRoot))
+            .Should()
+            .BeSealed()
+            .And()
+            .BeClasses()
+            .GetResult();
+
+        //Assert
+        Assert.True(testResult.IsSuccessful);
+    }
+}
--- src/modules/budgeting/tests/DomainLedger.Budgeting.ArchTests/InfrastructureTests.cs
+++ src/modules/budgeting/tests/DomainLedger.Budgeting.ArchTests/InfrastructureTests.cs
@@ -16,4 +16,18 @@ public class InfrastructureTests : TestBase
         Assert.True(testResult.IsSuccessful);
     }
 
+    [Fact]
+    public void Infrastructure_ShouldNotHave_Aggregates()
+    {
+        //Act
+        TestResult testResult = Types
+            .InAssembly(InfrastructureAssembly)
+            .Should()
+            .NotImplementInterface(typeof(IAggregateRoot))
+            .GetResult();
+
+        //Assert
+        Assert.True(testResult.IsSuccessful);
+    }
+
 }
